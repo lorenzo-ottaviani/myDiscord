595ddb4321c28a5743a1975406c47f6b2dc90c65
morgane-rossi <morgane.rossi@laplateforme.io>
2025-04-24 15:25:46 +0200

    xor cipher encryption

---
+++ xor_cipher.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* xor_encryption(char *src)
+{
+    /*
+    function that randomly creates a one bit integer as key
+    then a string character is encrypted with our key
+    */
+    int key = (rand() % 255) + 1 ;
+    int taille = strlen(src) ;
+
+    char *crypte = malloc(sizeof(char)* (taille +1));
+    if (crypte == NULL)
+    {
+        return NULL ;
+    }
+
+    int indice = 0 ;
+    while (src[indice] != '\0')
+    {
+        char ciph_letter = src[indice] ^ key ;
+        crypte[indice] = ciph_letter ;
+
+        indice ++ ;
+    }
+    crypte[taille-1] = '\0';
+    
+    return crypte ;
+}
+
+
+int main()
+{
+    char lettres[] = "sais-tu danser la carioca ?";
+    char *crypto = xor_encryption(lettres) ;
+    int taille = strlen(lettres);
+
+    for (int j = 0 ; j < taille ; j++)
+    {
+        printf("%d : %x", crypto[j], crypto[j]);
+    }
+    printf("\n");
+    free(crypto);
+
+    return 0 ;
+}
