f08d5d458cdc1dde498e579fc05da14c120982ed
nyxx <leila.wilde@laplateforme.io>
2025-04-28 09:25:25 +0100

    refactored & reorganised code

--- src/main.c
+++ src/main.c
@@ -16,34 +16,45 @@ static void activate(GtkApplication *app_inst, gpointer user_data) {
     gtk_widget_set_hexpand(stack, TRUE);
     gtk_widget_set_vexpand(stack, TRUE);
     
-    // Create the pages (widgets): login page, signup page, chat page.
-    // (Assuming you implement these functions in your respective modules.)
-    GtkWidget *login_page = create_login_page();   // from login.c
-    GtkWidget *signup_page = create_signup_page();   // from signup.c
-    GtkWidget *chat_page = create_chat_page();       // your chat page widget
+    // Create the pages (as widgets): login page, signup/register page, chat page.
+    GtkWidget *login_page = create_login_page();
+    GtkWidget *signup_page = create_signup_page();
+    GtkWidget *chat_page = create_chat_page();
     
     // Add each page to the stack with a unique name.
     gtk_stack_add_named(GTK_STACK(stack), login_page, "login");
     gtk_stack_add_named(GTK_STACK(stack), signup_page, "signup");
     gtk_stack_add_named(GTK_STACK(stack), chat_page, "chat");
     
-    // Optionally, you can add transition animations:
+    // Add transition animations:
     gtk_stack_set_transition_type(GTK_STACK(stack), GTK_STACK_TRANSITION_TYPE_SLIDE_LEFT_RIGHT);
     gtk_stack_set_transition_duration(GTK_STACK(stack), 300);
     
     // Set the stack as the child of the window.
     gtk_window_set_child(GTK_WINDOW(window), stack);
 
-    /* Set the initial page */
+    // Set the initial page.
     gtk_stack_set_visible_child_name(GTK_STACK(stack), "login");
     
     gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.github.leila-wilde.MyDiscord", 0);
+
+    GtkApplication *app;
+    int status;
+    
+    /* Create a new GtkApplication */
+    app = gtk_application_new("com.github.leila-wilde.MyDiscord", 0);
+    
+    /* Pass the app_widgets as user_data so it is accessible in activate */
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    
+    /* Run the application */
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+    
+    /* Cleanup */
     g_object_unref(app);
+    
     return status;
 }
