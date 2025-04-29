23122bee70a7b94c076901601fef484404c4b1a1
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-04-29 10:39:05 +0200

    feat: add cmake files and buid the server

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/server/src/create_database.c.obj"
+  "CMakeFiles/server.dir/server/src/create_database.c.obj.d"
+  "CMakeFiles/server.dir/server/src/main.c.obj"
+  "CMakeFiles/server.dir/server/src/main.c.obj.d"
+  "libserver.dll.a"
+  "server.exe"
+  "server.exe.manifest"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
